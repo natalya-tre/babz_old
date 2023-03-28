1c5dff83eca62a1cf8779c32d187fb98348232a1
MKRF\natalia.tretyakova <natalia@tretyakova>
2023-03-28 14:03:18 +0300

    start app

---
+++ src/colors/Colors.jsx
@@ -0,0 +1,32 @@
+import './colors.css';
+
+function Color() {
+    return (
+        <>
+            {/*<div className={'flex-container'}>*/}
+            {/*  <div className={'block midnightBlue'}></div>*/}
+            {/*  <div className={'block royalOrange'}></div>*/}
+            {/*  <div className={'block steelBlue'}></div>*/}
+            {/*  <div className={'block cadetBlue'}></div>*/}
+            {/*  <div className={'block forestGreen'}></div>*/}
+            {/*</div>*/}
+            {/*<div className={'flex-container'}>*/}
+            {/*  <div className={'block black'}></div>*/}
+            {/*  <div className={'block darkSlateGray'}></div>*/}
+            {/*  <div className={'block darkCyan'}></div>*/}
+            {/*  <div className={'block darkGrey'}></div>*/}
+            {/*  <div className={'block redBrown'}></div>*/}
+            {/*</div>*/}
+        <div style={{fontSize: '14px'}}>Набор цветов</div>
+                <div className={'flex-container'}>
+                    <div className={'block darkBlue'}></div>
+                    <div className={'block deepBlue'}></div>
+                    <div className={'block aqua'}></div>
+                    <div className={'block khaki'}></div>
+                    <div className={'block indianRed'}></div>
+                </div>
+        </>
+    );
+}
+
+export default Color;
